d6b9fc416412afcba6ac527872d13432946f41b8
jiin <jiin.kim109@gmail.com>
2021-03-15 16:50:00 -0700

    Added Url Response Caching

--- rest-api/app/Traits/ApiResponser.php
+++ rest-api/app/Traits/ApiResponser.php
@@ -4,6 +4,9 @@ namespace App\Traits;
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
 {
@@ -24,8 +27,11 @@ trait ApiResponser
         }
 
         $transformer = $collection->first()->transformer;
+        $collection = $this->filterData($collection);
         $collection = $this->sortData($collection);
+        $collection = $this->paginate($collection);
         $collection = $this->transformData($collection, $transformer);
+        $collection = $this->cacheResponse($collection);
 
         return $this->successResponse($collection, $code);
     }
@@ -54,5 +60,55 @@ trait ApiResponser
         $transformation = fractal($data, new $transformer);
 
         return $transformation->toArray();
+    }
+
+    protected function filterData(Collection $collection)
+    {
+        foreach (request()->query() as $query => $value) {
+            if (isset($collection->first()[$query]) && isset($value)) {
+                $collection = $collection->where($query, $value);
+            }
+        }
+
+        return $collection;
+    }
+
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50'
+        ];
+
+        Validator::validate(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+
+        $perPage = request()->has('per_page') ? (int) request()->per_page : 15;
+
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPage()
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
+    }
+
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+        $queryParams = request()->query();
+
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+
+        $fullUrl = "{$url}?{$queryString}";
+
+        return Cache::remember($fullUrl, 30, function () use ($data) {
+            return $data;
+        });
     }
 }
